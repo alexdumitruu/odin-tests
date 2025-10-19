e6c9f57d2655bb5771b2e59d8eaa6423a9237c9c
alexdumitruu <alex.dumitru.g@gmail.com>
2025-10-19 13:20:33 +0300

    done first function

--- practice.js
+++ practice.js
@@ -3,4 +3,12 @@
 // }
 // module.exports = sum;
 
+function capitalize(str) {
+    firstLetter = str.slice(0, 1).toUpperCase();
+    rest = str.substring(1)
+    return firstLetter + rest;
+}
 
+
+
+module.exports = capitalize;
--- practice.test.js
+++ practice.test.js
@@ -3,3 +3,9 @@
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum(1, 2)).toBe(3);
 // });
+
+const capitalize = require('./practice');
+
+test('first letter to be capitalized', () => {
+    expect(capitalize('tactu')).toBe('Tactu');
+});
